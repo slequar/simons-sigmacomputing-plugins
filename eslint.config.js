7eaa931829634bf5d939f2fbeaa036ad34217b86
Simon Lequar <39638743+slequar@users.noreply.github.com>
2025-10-19 13:56:44 -0700

    swap to yarn, further setup

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -9,15 +11,36 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    // ignores: ['vite.config.ts'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
+  // {
+  //   files: ['vite.config.ts'],
+  //   extends: [
+  //     js.configs.recommended,
+  //     tseslint.configs.recommended,
+  //     reactHooks.configs['recommended-latest'],
+  //     reactRefresh.configs.vite,
+  //   ],
+  //   languageOptions: {
+  //     ecmaVersion: 2020,
+  //     globals: globals.browser,
+  //   },
+  // },
 ])
