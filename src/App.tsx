18a16e2128ba109746a8570daab0739c6707f079
Simon Lequar <39638743+slequar@users.noreply.github.com>
2025-11-04 18:14:35 -0800

    Add Comparator plugin that fires action when listener matches test value

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Clock from "./plugins/clock";
 import OnLoad from "./plugins/onLoad";
 import Echo from "./plugins/echo";
+import Comparator from "./plugins/comparator";
 import Landing from "./pages/Landing";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/clock" element={<Clock />} />
         <Route path="/onload" element={<OnLoad />} />
         <Route path="/echo" element={<Echo />} />
+        <Route path="/comparator" element={<Comparator />} />
       </Routes>
     </Router>
   );
